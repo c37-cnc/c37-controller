c4ad9984db8d6379248826d75fc21e8b98d1154b
Ciro Cesar Maciel <ciro.maciel@c37.co>
2017-03-02 14:04:46 -0300

    deploy by gulp-github-deploy in Thu, 02 Mar 2017 17:04:46 GMT

--- src/js/library.js
+++ src/js/library.js
@@ -1,9 +1,9 @@
 /**
  *
- * C37 - Controller - Desktop in Thu Mar 02 2017 13:37:58 GMT-0300 (BRT)
+ * C37 - Controller - Desktop in Thu Mar 02 2017 14:03:55 GMT-0300 (BRT)
  *
  * c37-controller-desktop - C37 - Controlador - Desktop
- * @version 0.0.157
+ * @version 0.0.159
  * @link https://controller.c37.co/
  * @license CC BY-NC-ND 4.0
  *
